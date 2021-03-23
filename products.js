9fb2d2b4c347a8706554d9392bd4740d4d7141ff
George <gchriscarter@gmail.com>
2021-03-23 12:41:58 -0400

    add index, working but not correct show, and create to js and html

--- products.js
+++ products.js
@@ -5,6 +5,35 @@ var app = new Vue({
   data: function() {
     return {
       message: "Hello from JavaScript!",
+      toggleIndex: false,
+      products: [],
+      singleProduct: "",
+      input: "",
+      name: "",
+      description: "",
+      price: "",
+      image_url: "",
     };
   },
+  methods: {
+    index: function () {
+      axios.get( "http://localhost:3000/api/products").then(response => {
+        console.log(response.data);
+        this.products = response.data;
+        this.toggleIndex = !this.toggleIndex;
+      });
+    },
+    show: function () {
+      console.log(parseInt(this.input));
+      axios.get( "http://localhost:3000/api/products/" + this.input).then(response => {
+        this.singleProduct = response.data;
+      });
+    },
+    createProduct: function() {
+      axios.post("http://localhost:3000/api/products", {name: this.name, price: this.price, description: this.description, image_url: this.image_url}).then(response => {
+        console.log(response.data);
+      })
+    }
+  }
 });
+
